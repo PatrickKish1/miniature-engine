f63a9121f7986bc48a322bbbf7e41017445301c0
PatrickKish1 <patrickkesh90@gmail.com>
2024-09-01 11:42:40 -0800

    Updating files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import Header from './components/Header'; // Ensure the path is correct
 import Footer from './components/Footer';
 
 function App() {
+  // Function to handle button click
+  const handleRedirect = () => {
+    window.location.href = 'https://equibloc.vercel.app/';
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow flex flex-col items-center justify-center">
         {/* Main content goes here */}
+        <button
+          onClick={handleRedirect}
+          className="bg-[#ff0909] text-white px-4 py-2 rounded mt-4"
+        >
+          Go to Equibloc
+        </button>
       </main>
       <Footer />
     </div>
